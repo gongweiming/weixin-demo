67a39d10b1d17e39118c0c74ff327bd9b8af6a75
1316101845@qq.com <1316101845@qq.com>
2018-04-07 23:00:55 +0800

    测试第一次提交代码

--- src/main/java/com/cn/controller/WeiXinController.java
+++ src/main/java/com/cn/controller/WeiXinController.java
@@ -24,8 +24,9 @@ public class WeiXinController {
     @RequestMapping("/init")
     public void init(HttpServletRequest request, HttpServletResponse response){
         System.out.println("init...");
-        //		signature	微信加密签名，signature结合了开发者填写的token参数和请求中的timestamp参数、nonce参数。
         //		timestamp	时间戳
+        //		signature	微信加密签名，signature结合了开发者填写的token参数和请求中的timestamp参数、nonce参数。
+        System.out.println("init...");
         //		nonce	随机数
         //		echostr
         String signature = request.getParameter("signature");
